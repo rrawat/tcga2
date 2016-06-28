acaefbe777e0ab3324d9bb99149d501f94e437b8
rrawat <rawat.radhika@gmail.com>
2016-06-28 18:55:49 -0500

    Matches cosmic mutants to 1/2 of exon database, for BRCA2. Returns list of exons and mutations that are an exact match by transcript name, chromosome, start, and end. Creates file of exons and mutations that match by transcript name.

--- splice_mutations.py
+++ splice_mutations.py
@@ -1,21 +1,28 @@
 
+from datetime import datetime
 
 def importdata(path, delimiter, length=10000000000000, condition=""):
+    start=datetime.now()
     f=open(path)
     data=[]
     i=0
     for row in f:
-        if i <= length - 1:
+        if i <= length:
             if condition !="":
                 if condition in str(row):
                     data.append(row.split(delimiter))
                     i += 1
+
             else:
                 data.append(row.split(delimiter))
                 i += 1
+
+        else: break
+
     titles=data[0]
     data=data[1:]
-    print path, "finished importing"
+    print path, "finished importing. Length:",len(data) ,". Elapsed time:",datetime.now()-start,
+    f.close()
     return titles, data
 
 
@@ -23,16 +30,32 @@ CosmicMutantsPath="/Users/radhikarawat/Desktop/Programming/TCGA_projects/CosmicM
 ExonPath="/Users/radhikarawat/Downloads/wgEncodeGencodeExonSupportV20.txt"
 
 
-CosmicMutants_data =importdata(CosmicMutantsPath,'\t',condition="BRCA2")[1]
-Exon_data= importdata(ExonPath,'\t')[1]
+CosmicMutants_data =importdata(CosmicMutantsPath,'\t',condition="BRCA2",length=100)[1]
+Exon_data= importdata(ExonPath,'\t',length=8000000)[1]
 
 i=0
+length=len(Exon_data)
+start=datetime.now()
+
+
+f=open("/Users/radhikarawat/PycharmProjects/TCGA2/ExonData.csv","w+")
 for Exon in Exon_data:
     i+=1
+    #print i, "out of", length, "elapsed time:", datetime.now()-start
     for CosmicMutant in CosmicMutants_data:
-        #print i
-        if Exon[0].split(".")[0] in str(CosmicMutant):
-            ##Exon.append(CosmicMutant[0])
-            print i, "A", Exon
-        if CosmicMutant[1] in str(Exon):
-            print i, "B", Exon
+        if "-" in str(CosmicMutant[23]):
+            if Exon[0].split(".")[0] in str(CosmicMutant):
+
+                line=str(Exon)+','+str(CosmicMutant)+"\n"
+                f.write(line)
+
+                #condition 1
+                ExonChromosome=Exon[4].split("chr"[1])
+                ExonStart=Exon[5]
+                ExonEnd=Exon[6].split('\n')[0]
+                MutantChromosome,MutantEnd=CosmicMutant[23].split("-")
+                MutantChromosome,MutantStart=MutantChromosome.split(":")
+                if [ExonChromosome,ExonStart,ExonEnd]==[MutantChromosome,MutantStart,MutantEnd]:
+                    print Exon, CosmicMutant, i, "out of", length, "elapsed time:", datetime.now()-start
+            #Exon.append(CosmicMutant[0])
+f.close()
---
+++ test.py
@@ -0,0 +1,7 @@
+b=[1,2,3,4,5,6,7,8,9,0]
+i=0
+for letter in b:
+    while i<2:
+        print i
+        i+=1
+        print i
